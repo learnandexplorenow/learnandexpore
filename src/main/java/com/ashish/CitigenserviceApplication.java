8f7eb0a5d5fc4ee18ebd3b6a15a488b2ca725676
91824 <91824@LAPTOP-UMB6DISO>
2022-12-28 12:57:31 +0530

    FIrst commmit

---
+++ src/main/java/com/ashish/CitigenserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ashish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitigenserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitigenserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashish/CitigenserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitigenserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
